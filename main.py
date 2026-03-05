6890cea3ec92f1d26ce83b87810d22621456fbf8
Jcmedinah <jean.medina0399@gmail.com>
2026-03-04 21:20:37 -0500

    Feature: creacion de menu

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 
 def main():
-    pass
+    menu = True
+    while menu:
+        print('Gestion de Libros Edulend\n' \
+        '1. Ingresar libro\n' \
+        '2. Actualizar cantidades\n' \
+        '3. Mostrar inventario\n' \
+        '4. Buscar libro por nombre\n' \
+        '5. Salir.')
+        opcion = int(input('Selecciona una opcion:'))
 
 if __name__ == '__main__':
     main()
