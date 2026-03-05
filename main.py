ca254b8dca03da94d6bd4f9a88f8e4f1f6b3f7bb
Jcmedinah <jean.medina0399@gmail.com>
2026-03-04 21:22:21 -0500

    Feature: se crea el match para las opciones y el cerrar menu

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ def main():
         '4. Buscar libro por nombre\n' \
         '5. Salir.')
         opcion = int(input('Selecciona una opcion:'))
-
+        match opcion:
+            case 1:
+                print('Ingresar libro')
+            case 2:
+                print('Actualizar cantidades')
+            case 3:
+                print('Mostrar inventario')
+            case 4:
+                print('Buscar libro por nombre')
+            case 5:
+                print('Saliendo')
+                menu = False
 if __name__ == '__main__':
     main()
